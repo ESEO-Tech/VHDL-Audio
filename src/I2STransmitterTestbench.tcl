ced2dc8f93a5461c415c6a74848a22cecb58f32d
Guillaume Savaton <guillaume.savaton@eseo.fr>
2021-02-16 14:22:25 +0100

    I2S : ajout du script de simulation

---
+++ src/I2STransmitterTestbench.tcl
@@ -0,0 +1,20 @@
+
+# Close simulator if a simulation session is already open.
+if {[current_sim] ne ""} {
+    close_sim -force
+}
+
+# Set top module.
+set_property top I2STransmitterTestbench [get_filesets sim_1]
+
+# Start the simulator.
+launch_simulation
+restart
+
+# Add waves for signals in I2STransmitter.
+set tbdiv [add_wave_divider -before_wave clk_sim I2STransmitterTestbench]
+set div   [add_wave_divider -before_wave $tbdiv  I2STransmitter]
+add_wave -after_wave $div transmitter_inst/*
+
+# Run.
+run 200us
